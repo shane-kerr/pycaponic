01630836fc347c9ad1839c57153b96a2d5679c64
Shane Kerr <shane@time-travellers.org>
2017-08-06 23:38:45 +0200

    Comment our example.

--- examples/findbroadcast.py
+++ examples/findbroadcast.py
@@ -1,4 +1,24 @@
+"""
+This is a sample program which identifies the broadcast packets from
+a pcap/PcapNG file.
+
+If looking at a PcapNG, then the Enhanced Packet Block flags option is
+checked (if it exists). If the packet reception is specified as
+broadcast, then the packet is treated as broadcast.
+
+If the source is pcap, or if no flags option is present, or if the
+reception is not broadcast then the destination MAC address of the
+packet is checked. If it is set to all-ones (FF-FF-FF-FF-FF-FF) then
+the packet is treated as broadcast. (We also check for 64-bit all ones
+MAC address, FF-FF-FF-FF-FF-FF-FF-FF, even though no link layer
+encapsulation that we support actually reports 64-bit MAC addresses.)
+
+For each packet, we print the time it arrive and whether it was a
+broadcast packet or not. If it was broadcast, we also print the packet
+type.
+"""
 import sys
+import time
 
 # Normally you can just use "import pycaponic", but if we cannot
 # import it, try again adding the parent directory. This lets us run
@@ -14,27 +34,43 @@ except ImportError:
         import pycaponic
 
 def process(handle):
+    """
+    Process the packets from the file handle.
+    """
     for packet in pycaponic.packets(handle):
+        # packet.timestamp is the epoch time - the seconds since
+        # 1970-01-01T00:00:00. It's a bit easier to read as an ISO
+        # timestamp, and we also add the fractional seconds properly
+        # with this utility function.
+        when = pycaponic.epoch2iso8601(packet.timestamp)
+
+        # Next try to figure out if this is a broadcast packet.
         broadcast = False
+        # Check how it was received, if we know.
         if packet.epb_flags is not None:
             if packet.epb_flags.reception == "broadcast":
                 broadcast = True
+        # Otherwise see if the MAC address is all-ones.
         if packet.mac_dst in ("FF-FF-FF-FF-FF-FF", "FF-FF-FF-FF-FF-FF-FF-FF"):
             broadcast = True
+        # Finally output whatever we discovered about each packet.
         if broadcast:
-            print("%s -> BROADCAST" % packet.timestamp)
+            print("%s -> BROADCAST (%s)" % (when, packet.pkttype))
         else:
-            print("%s -> not broadcast" % packet.timestamp)
+            print("%s -> not broadcast" % when)
 
 def main():
-    # get the list of files
+    # Get the list of files from the arguments.
     files = sys.argv[1:]
-    # if not files were specified, read from stdin
+    # If no files were specified, read from stdin only.
     if not files:
         files = "-"
-    # process each file separately
+    # Process each file separately.
     for fname in files:
         if fname == "-":
+            # We use "sys.stdin.buffer" for stdin because we need the
+            # binary input stream. If performance is an issue then we
+            # could assign a new, binary buffer to it.
             process(sys.stdin.buffer)
         else:
             with open(fname, "rb") as handle:
