46a3c656155bbcfea1dc302f6c992e92c46654f1
Shane Kerr <shane@time-travellers.org>
2017-08-07 20:44:19 +0200

    Export the epoch2iso8601 function

--- pycaponic/__init__.py
+++ pycaponic/__init__.py
@@ -3,4 +3,4 @@ __version__ = '0.0.1'
 __author__ = 'Shane Kerr'
 
 from pycaponic.pycaponicError import pycaponicError
-from pycaponic.EncapsulatedPacket import packets
+from pycaponic.EncapsulatedPacket import packets, epoch2iso8601
