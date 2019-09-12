0c858d3ac68bfb212b11db74a948baa2cd30455b
Shane Kerr <skerr@ns1.com>
2019-09-12 21:47:36 +0200

    Add support for decoding Linux SLL pcap files

--- pycaponic/linklayer.py
+++ pycaponic/linklayer.py
@@ -17,6 +17,7 @@ LINKTYPE_NULL = 0
 LINKTYPE_ETHERNET = 1
 LINKTYPE_RAW = 101
 LINKTYPE_LOOP = 108
+LINKTYPE_LINUX_SLL = 113
 LINKTYPE_IPV4 = 228
 LINKTYPE_IPV6 = 229
 
@@ -27,6 +28,7 @@ LINKTYPES = {
     LINKTYPE_LOOP: "OpenBSD loopback encapsulation",
     LINKTYPE_IPV4: "IPv4",
     LINKTYPE_IPV6: "IPv6",
+    LINKTYPE_LINUX_SLL: "Linux cooked-mode capture (SLL)",
 }
 
 
@@ -96,6 +98,72 @@ def decode_ipv6(_, link_pkt):
     return link_pkt, "IPv6", {}
 
 
+# Values documented in the packet(7) man page, pulled from the
+# linux/if_packet.h header.
+SLL_PKT_TYPES = {
+    0: 'PACKET_HOST',
+    1: 'PACKET_BROADCAST',
+    2: 'PACKET_MULTICAST',
+    3: 'PACKET_OTHERHOST',
+    4: 'PACKET_OUTGOING',
+}
+
+
+# Select values pulled from the if_arp.h header.
+LINUX_ARPHDR_TYPES = {
+    1: "ARPHDR_ETHER",
+    768: "ARPHDR_TUNNEL",
+    769: "ARPHDR_TUNNEL6",
+    772: "ARPHDR_LOOPBACK",
+    776: "ARPHDR_SIT",
+    823: "ARPHDR_IP6GRE",
+    824: "ARPHDR_NETLINK",
+    825: "ARPHDR_6LOWPAN",
+}
+
+
+def decode_linux_sll(_, link_pkt):
+    (sll_pkttype_val,
+     sll_hatype_val,
+     sll_halen) = struct.unpack("!HHH", link_pkt[0:6])
+    sll_addr = link_pkt[6:14]
+    sll_addr = sll_addr[:sll_halen]
+    sll_addr = '-'.join(["%02X" % octet for octet in sll_addr])
+    sll_protocol, = struct.unpack("!H", link_pkt[14:16])
+    try:
+        sll_pkttype = SLL_PKT_TYPES[sll_pkttype_val]
+    except KeyError:
+        msg = "unknown type {} of Linux SLL packet".format(sll_pkttype)
+        raise LinkLayerError(msg)
+    try:
+        sll_hatype = LINUX_ARPHDR_TYPES[sll_hatype_val]
+    except KeyError:
+        sll_hatype = "Unknown ARPHDR type " + str(sll_hatype_val)
+
+    if sll_protocol == ETHERTYPE_IPv4:
+        pkt_type = "IPv4"
+        sll_protocol = 'ETHERTYPE_IPv4'
+    elif sll_protocol == ETHERTYPE_IPv6:
+        pkt_type = "IPv6"
+        sll_protocol = 'ETHERTYPE_IPv6'
+    elif sll_protocol == ETHERTYPE_ARP:
+        pkt_type = "ARP"
+        sll_protocol = 'ETHERTYPE_ARP'
+    else:
+        pkt_type = "EtherType 0x%04X" % sll_protocol
+        sll_protocol = str(sll_protocol)
+
+    info = {
+        'sll_pkttype': sll_pkttype,
+        'sll_hatype': sll_hatype,
+        'sll_halen': sll_halen,
+        'sll_addr': sll_addr,
+        'sll_protocol': sll_protocol,
+    }
+
+    return link_pkt[16:], pkt_type, info
+
+
 LINK_DECODERS = {
     LINKTYPE_NULL: decode_null,
     LINKTYPE_ETHERNET: decode_ethernet,
@@ -103,6 +171,7 @@ LINK_DECODERS = {
     LINKTYPE_LOOP: decode_loop,
     LINKTYPE_IPV4: decode_ipv4,
     LINKTYPE_IPV6: decode_ipv6,
+    LINKTYPE_LINUX_SLL: decode_linux_sll,
 }
 
 
@@ -110,7 +179,7 @@ class Decoder:
     def __init__(self, link_type, byte_order):
         decoder = LINK_DECODERS.get(link_type)
         if not decoder:
-            raise LinkLayerError("unknown link type")
+            raise LinkLayerError("unknown link type #" + str(link_type))
         self.decoder = decoder
         self.byte_order = byte_order
 
